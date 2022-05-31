d3ae02a34548c6ce5416e602613a56f0fa7ba54e
Sebastian Bolanos <sebasheston@protonmail.com>
2022-05-31 11:23:31 +0100

    Init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    nixos-generators,
+    ...
+  }: {
+    packages.x86_64-linux = {
+      do = nixos-generators.nixosGenerate {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          # you can include your own nixos configuration here, i.e.
+          # ./configuration.nix
+        ];
+        format = "do";
+      };
+    };
+  };
+}
